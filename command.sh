8e0dae6e0cdb10ac12b3b748228cc4752a27bf09
AbhishekKatara <abhikatara.sanskar91@gmail.com>
2018-01-05 11:51:01 +0530

    Requirements text added

---
+++ command.sh
@@ -0,0 +1,8 @@
+pip install -r floyd_requirements.txt
+cp -r /code/* /output
+cd /output
+echo "[program:tensorboard]" > /etc/supervisor/conf.d/tensorboard.conf
+echo "command=tensorboard --port 6001 --logdir=/output" >> /etc/supervisor/conf.d/tensorboard.conf
+echo "autorestart=true" >> /etc/supervisor/conf.d/tensorboard.conf
+echo "startretries=10000" >> /etc/supervisor/conf.d/tensorboard.conf
+service supervisor start || true; /run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/HQgD9JP4DgJ4kAPWyaggsb' --NotebookApp.token='' --NotebookApp.allow_origin='*' --NotebookApp.tornado_settings="{'headers': {'Content-Security-Policy': \"frame-ancestors 'self' www.floydhub.com \"}}" --NotebookApp.iopub_data_rate_limit=1.0e10
